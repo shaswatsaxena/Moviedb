0a2527804db19ff7d3ae95756d4528bf2405aada
shaswatsaxena <shaswatsaxena@gmail.com>
2018-11-22 20:37:37 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express'),
+    request = require('request');
+    app = express(),
+    port = process.env.PORT;
+
+var resultobj ;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.set("view engine", "ejs");
+
+app.get('/',  (req, res) =>   res.render('home') );
+
+app.get('/result',  function(req, res) {
+    let searchTerm = req.query.search;
+    let page = Number(req.query.page) ? Number(req.query.page) : 1;
+    let apiURL = "http://www.omdbapi.com/?apikey=thewdb&s="+searchTerm+"&page="+page ;
+    request(apiURL, function (error, response, body) {
+        if (error)
+            res.send('ERROR:', error);
+        else {
+            console.log('Status Code:', response && response.statusCode);
+            resultobj = JSON.parse(body);
+            resultobj.searchTerm = searchTerm;
+            resultobj.currentPage = page;
+            res.render('results' , {resultobj : resultobj});
+        }
+    });
+});
+
+app.listen(port, () => console.log(`Movie API App listening on port ${port}!`));
