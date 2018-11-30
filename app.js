9610f4309807a5783b8780b3f68879ec98b9c14a
shaswatsaxena <shaswatsaxena@gmail.com>
2018-11-30 19:13:34 +0530

    Added Subtitles feature

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const express = require('express'),
-    request = require('request');
-    app = express(),
-    port = process.env.PORT;
+const  express = require('express'),
+            request = require('request');
+            app = express(),
+            bodyParser = require('body-parser'),
+            port = process.env.PORT;
+
 
 var resultobj ;
 if (port == null || port == "") {
   port = 3000;
 }
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/',  (req, res) =>   res.render('home') );
+//===================================
+//                  ROUTES
+//===================================
+app.get('/', (req, res) =>   res.render('home') );
 
-app.get('/result',  function(req, res) {
+app.get('/result', function(req, res) {
     let searchTerm = req.query.search;
     let page = Number(req.query.page) ? Number(req.query.page) : 1;
     let apiURL = "http://www.omdbapi.com/?apikey=thewdb&s="+searchTerm+"&page="+page ;
