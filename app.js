8aa0bbf23c05030f4b2d7d675976f3a82b748ebb
shaswatsaxena <shaswatsaxena@gmail.com>
2019-02-17 22:05:08 +0530

    Changed View Engine to Pug

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
-const  express = require('express'),
-            request = require('request');
-            app = express(),
-            bodyParser = require('body-parser'),
-            port = process.env.PORT;
+const express = require("express"),
+  request = require("request");
+(app = express()),
+  (bodyParser = require("body-parser")),
+  (port = process.env.PORT);
 
-
-var resultobj ;
 if (port == null || port == "") {
   port = 3000;
 }
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //===================================
 //                  ROUTES
 //===================================
-app.get('/', (req, res) =>   res.render('home') );
+app.get("/", (req, res) => res.render("home"));
 
-app.get('/result', function(req, res) {
-    let searchTerm = req.query.search;
-    let page = Number(req.query.page) ? Number(req.query.page) : 1;
-    let apiURL = "http://www.omdbapi.com/?apikey=thewdb&s="+searchTerm+"&page="+page ;
-    request(apiURL, function (error, response, body) {
-        if (error)
-            res.send('ERROR:', error);
-        else {
-            console.log('Status Code:', response && response.statusCode);
-            resultobj = JSON.parse(body);
-            resultobj.searchTerm = searchTerm;
-            resultobj.currentPage = page;
-            res.render('results' , {resultobj : resultobj});
-        }
-    });
+app.get("/result", function(req, res) {
+  let searchTerm = req.query.search;
+  let page = Number(req.query.page) ? Number(req.query.page) : 1;
+  let apiURL =
+    "http://www.omdbapi.com/?apikey=thewdb&s=" + searchTerm + "&page=" + page;
+  request(apiURL, function(error, response, body) {
+    if (error) res.send("ERROR:", error);
+    else {
+      console.log("Status Code:", response && response.statusCode);
+      const allMedia = JSON.parse(body);
+      if (allMedia.Response === "True")
+        res.render("results", { allMedia, searchTerm, page });
+      else res.render("noResult", { searchTerm });
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Movie API App listening on port ${port}!`));
